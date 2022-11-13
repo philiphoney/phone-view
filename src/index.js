fc0b61edf99f81a32cbbcdd221917f21cc0f8e57
RedofFas <Philip.stuessel@icloud.com>
2022-11-13 20:39:19 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ function createModel(model) {
   console.log(model)
   if (model == "iPhone_14_pro") {
 document.getElementById("view").innerHTML = `
-<style> #view-bar {    padding-top: 50px;    display: flex;    width: 500px;    justify-content: space-around;    background-color: white;}#view-phone {    width: 500px;    justify-content: center;    display: flex;    position: absolute;    z-index: 5;} #view-ifram {    padding-top: 90px;    height: 900.7px;    display: flex;;    z-index: 10;    position: absolute;    justify-content: center;}iframe {    background-color: white;    border-radius: 0px 0px 60px 60px;    width: 447.3px;}#time {    padding-left: 30px;    font-size: 20px;    font-family: Arial, Helvetica, sans-serif;    font-weight: bolder;    padding-bottom: 20px;}#status-bar-hero {  width: 100px;  padding-left: 90px}
+<style> #view-bar {    padding-top: 50px;    display: flex;    width: 500px;    justify-content: space-around;    background-color: white;}#view-phone {    width: 500px;    justify-content: center;    display: flex;    position: absolute;    z-index: 5;} #view-ifram {    padding-top: 90px;    height: 900.7px;    display: flex;;    z-index: 10;    position: absolute;    justify-content: center;}iframe {    background-color: white;    border-radius: 0px 0px 60px 60px;    width: 447.5px;}#time {    padding-left: 30px;    font-size: 20px;    font-family: Arial, Helvetica, sans-serif;    font-weight: bolder;    padding-bottom: 20px;}#status-bar-hero {  width: 100px;  padding-left: 90px}
 </style>
 <img id="view-phone" src="./phonemodels/IPhone_14_Pro.png" alt="">
 <div id="view-bar"><div id="time"><span class="moodyTime"></span></div><div><img id="status-bar-hero" src="./phonemodels/status-bar-hero.png" alt=""></div></div>
